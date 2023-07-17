bf31be850576c4627a6a62e0c3c181dfe505ab56
Max Bueckert <maxbueckert@gmail.com>
2023-07-17 14:13:45 -0700

    Update Title.js

--- amplified_todo/src/components/Title.js
+++ amplified_todo/src/components/Title.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
-// import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 import { useNavigation } from '@react-navigation/native';
 
